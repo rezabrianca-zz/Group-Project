85eb77b8d0409db58231c4fd3b92b41b9d647a6a
Justin Leiendecker <Justin Leiendecker>
2016-09-27 17:06:06 +0100

    Our dataframe

---
+++ dataframe.R
@@ -0,0 +1,26 @@
+
+df <- read.csv("shot_logs.csv", header = TRUE)
+
+GameID <- df[,1]
+MatchUp <- df[,2]
+Location <- df[,3]
+WinLoss <- df[,4]
+FinalMargin <- df[,5]
+ShotNumber <- df[,6]
+Period <- df[,7]
+GameClock <- df[,8]
+ShotClock <- df[,9]
+Dribbles <- df[,10]
+TouchTime <- df[,11]
+ShotDistance <- df[,12]
+PointType <- df[,13]
+ShotResult <- df[,14]
+ClosestDefenderName <- df[,15]
+ClosestDefenderID <- df[,16]
+ClosestDefenderDistance <- df[,17]
+FieldGoalMade <- df[,18]
+Points <- df[,19]
+PlayerName <- df[,20]
+PlayerID <- df[,21]
+
+summary(df)
